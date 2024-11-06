b6346eeedb9c1a43fd8a31a8e3b4fadb9d6076e6
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-06 17:47:39 +0100

    project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,17 @@ class ProjectSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
+
     public function run(): void
     {
-        //
+        $project = new Project();
+        $project->title = "BoolFlix";
+        $project->author = "Tommaso-Panarotto";
+        $project->language = "JS";
+        $project->url = "https://github.com/Tommaso-Panarotto/vite-boolflix";
+        $project->description = "Simulazione di una libreria stile netfl";
+        $project->type_id = 1;
+        $project->save();
     }
 }
