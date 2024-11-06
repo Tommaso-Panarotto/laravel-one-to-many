08bc91accfa05c01e686a5f19f6d37f805690d15
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-06 18:54:19 +0100

    added type_id to form request project store

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -12,7 +12,7 @@ class StoreProjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        if (Auth::check()){
+        if (Auth::check()) {
             return true;
         }
         return false;
@@ -30,7 +30,8 @@ class StoreProjectRequest extends FormRequest
             "author" => ["required", "string", "min:3", "max:40"],
             "language" => ["required", "string", "min:1", "max:20"],
             "url" => ["required", "url"],
-            "description" => ["required", "string", "min:10"]
+            "description" => ["required", "string", "min:10"],
+            "type_id" => ["required", "numeric", "integer", "exists:types,id"]
         ];
     }
 }
